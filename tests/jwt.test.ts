79750d081dd6b81e3ead625d3bc97f759d98e32c
Ismail Mohammed <ismail.mohammed@pipedrive.com>
2022-11-09 18:07:24 +0200

    feat: added tests for different alrithms

--- tests/jwt.test.ts
+++ tests/jwt.test.ts
@@ -1,7 +1,7 @@
 import { suite } from 'uvu'
 import { ServerResponse as Response } from 'http'
 import fs from 'fs'
-import jsonwebtoken from 'jsonwebtoken'
+import jsonwebtoken, { Algorithm } from 'jsonwebtoken'
 import expect from 'expect'
 import { jwt, Request } from '../src/index'
 
@@ -15,7 +15,7 @@ it.before.each(() => {
   req.user = {}
 })
 
-it('should work if authorization header is valid jsonwebtoken', () => {
+it('should work using default algorithm if authorization header is valid jsonwebtoken', () => {
   const secret = 'shhhhhh'
   const token = jsonwebtoken.sign({ foo: 'bar' }, secret)
 
@@ -24,6 +24,20 @@ it('should work if authorization header is valid jsonwebtoken', () => {
   jwt({ secret: secret })(req, res, () => {
     expect(req.user.foo).toBe('bar')
   })
+})
+
+it('should work with different HMAC algorithms', () => {
+  const algorithms: Algorithm[] = ['HS512', 'HS256', 'HS384']
+  const secret = 'shhhhhh'
+
+  algorithms.forEach(algorithm => {
+    const token = jsonwebtoken.sign({ foo: 'bar' }, secret, { algorithm })
+    req.headers.authorization = 'Bearer ' + token
+
+    jwt({ secret: secret, algorithm })(req, res, () => {
+      expect(req.user.foo).toBe('bar')
+    })
+  });
 })
 
 it('should work if authorization header is valid with a buffer secret', () => {
@@ -46,6 +60,20 @@ it('should handle private key encryption', () => {
   jwt({ secret: [privateKey, publicKey], algorithm: 'RS256' })(req, res, () => {
     expect(req.user.foo).toBe('bar')
   })
+})
+
+it('should work with different RSA algorithms', () => {
+  const algorithms: Algorithm[] = ['RS256', 'RS384', 'RS512']
+  const privateKey = fs.readFileSync('tests/fixtures/private', { encoding: 'utf-8' })
+  const publicKey = fs.readFileSync('tests/fixtures/public', { encoding: 'utf-8' })
+
+  algorithms.forEach(algorithm => {
+    req.headers.authorization = 'Bearer ' + jsonwebtoken.sign({ foo: 'bar' }, privateKey, { algorithm: 'RS256' })
+
+    jwt({ secret: [privateKey, publicKey], algorithm })(req, res, () => {
+      expect(req.user.foo).toBe('bar')
+    })
+  });
 })
 
 it('should not work with malformed input', () => {
